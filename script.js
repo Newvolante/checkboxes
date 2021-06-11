f6462052fddd6bed9f8d12f5e71202e2cdc98d50
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-06-11 13:01:02 +0200

    selecting the select all options selects all the options

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 (function() {
     console.warn('Script correctly linked to web page');
+
+    let selectAllOptions = document.getElementById('all');
+    let optionsNodelist = document.forms[0].elements.option;
+    
+    
+    selectAllOptions.addEventListener('change', function() {
+        let allOptionsState = selectAllOptions.checked;
+
+        console.log(allOptionsState);
+
+        if (allOptionsState) {
+            optionsNodelist.forEach(item => { item.checked = true; });
+        }
+    });
+
 })();
