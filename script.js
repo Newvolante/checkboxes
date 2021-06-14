a09ca48299c1bed041b7811795b212347a18a4f9
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-06-14 11:54:30 +0200

    added explicative text - changed variable name

--- script.js
+++ script.js
@@ -4,26 +4,30 @@
     let selectAllOptions = document.getElementById('all');
     let optionsNodelist = document.forms[0].elements.option;
     
-    
+    // adding an event listener to the "select all" checkbox
     selectAllOptions.addEventListener('change', function() {
+        
+        // "select all" checkbox state
         let allOptionsState = selectAllOptions.checked;
 
-        console.log('select all checkboxes: ' + allOptionsState);
-
+        // sets the checked status of each checkbox equal to the checked status
+        // of the "select all" checkbox
         optionsNodelist.forEach(item => {
             item.checked = allOptionsState;
         })
     });
 
+    // adding an event listener to the checkboxes but the "select all" checkbox
     optionsNodelist.forEach(item => {
         item.addEventListener('change', function() {
-            let allORNot = Array.from(optionsNodelist).every(item => {
+            
+            // checks if every checkbox but the "select all" one is checked
+            let allchecked = Array.from(optionsNodelist).every(item => {
                 return item.checked == true;
             })
             
-            if (allORNot == false) {
-                selectAllOptions.checked = false;
-            } else selectAllOptions.checked = true;
+            // "select all" checkbox only checked if all the options are checked
+            allchecked == false ? selectAllOptions.checked = false : selectAllOptions.checked = true;
         });
     });
 })();
